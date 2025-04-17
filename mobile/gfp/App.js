ecc7c1e81d49c0af5bffa50aca1eedb21983b90c
Laiza <laizac981@gmail.com>
2025-04-17 15:20:26 -0300

    Criação e configuração do projeto GFP

---
+++ mobile/gfp/App.js
@@ -0,0 +1,19 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack"; 
+import Login from "./src/pages/Login.js";
+import MenuDrawer from "./src/pages/MenuDrawer.js"; 
+
+const Stack = createNativeStackNavigator(); 
+
+export default function App() {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="MenuDrawer" component={MenuDrawer} 
+        options={{ headerShow: false}} />  
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
